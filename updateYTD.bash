a8b89088fac6a5cf4982bcb6043e484297a206e5
Mason You <masonyou.su@gmail.com>
2021-06-22 22:51:08 -0400

    fixed bug in updateYTD.bash

--- updateYTD.bash
+++ updateYTD.bash
@@ -2,13 +2,14 @@
 
 set -e
 
-
 old_ver=$(python youtube-dl --version)
 
-wget -c https://youtube-dl.org/downloads/latest/youtube-dl
+rm -fv youtube-dl
+
+wget -c https://youtube-dl.org/downloads/latest/youtube-dl > /dev/null
 
 new_ver=$(python youtube-dl --version)
 
 git ci youtube-dl -m "updated youtube-dl from $old_ver to $new_ver"
 
-# git push
+git push
